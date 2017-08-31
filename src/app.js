d4297689e5bee1d83624a244969b5676e26728cf
Jamie Nuttall <jamie@wattu.com>
2017-08-31 14:58:38 +0100

    added IP connect logic

--- src/app.js
+++ src/app.js
@@ -1,16 +1,36 @@
-import { startPing } from './car_service'
+import { startPing, sendMessage } from './car_service'
 import { Observable } from 'rxjs'
+const net = require('net');
 
-const pingSub = startPing(3000, 5000)
-.retryWhen(errors => errors
-    //log error message
-    .do(err => console.log('Ping ' + err + ' restarting in 5 seconds'))
-    //restart in 5 seconds
-    .delayWhen(val => Observable.timer(5000))
-)
-.subscribe(x => {
-    x.subscribe(y => console.log('+++' + y), err => {
-        console.log('Error1 ' + err)
-        pingSub.unsubscribe()
-    })
-})
+const client = new net.Socket();
+
+const connect = (cmd,cb) => {
+	if(!client.writeable) {
+		console.log('Connecting');
+		client.connect(8080,'192.168.8.46', ()=> {
+		});
+
+		client.on('data',(data) => {
+            console.log(data.toString('hex'));
+            sendMessage(data)
+		});
+		client.once('connect',() => {
+			console.log('Connected');		
+                	cb();
+
+		});
+		client.on('error',(err) => {
+			console.log('error ' + err);
+		});
+		client.on('end',() => {
+			console.log('end');
+		});
+	} else {
+		comsole.log('Sending');
+		cb();
+	}
+}
+
+connect(0,()=>{
+
+});
