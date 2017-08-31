f9f83597fbee2b12c2d553b56ede60e41512565e
Jamie Nuttall <jamie@wattu.com>
2017-08-31 18:29:03 +0100

    refactor to observable

--- src/app.js
+++ src/app.js
@@ -8,31 +8,14 @@ const log = message => logging ? console.log(message) : undefined
 
 const client = new net.Socket();
 
-const connect = (cmd,cb) => {
-	if(!client.writeable) {
-		log('Connecting');
-		client.connect(8080,'192.168.8.46', ()=> {
-		})
-
-		client.on('data',(data) => {
-            send('phev/receive',data)
+const connect = (cmd, cb) => {
+    log('Connecting');
+    client.connect(8080, '192.168.8.46', () => {
+        Observable.fromEvent(client,'data').subscribe(data => {
+            send('phev/receive', data)
             log('Car    : ' + data.toString('hex'))
         })
-		client.once('connect',() => {
-			log('Connected');		
-                	cb();
-
-		})
-		client.on('error',(err) => {
-			log('error ' + err);
-		});
-		client.on('end',() => {
-			log('end');
-		});
-	} else {
-		log('Sending');
-		cb();
-	}
+    })
 }
 
 messages().subscribe(m => {
@@ -40,6 +23,6 @@ messages().subscribe(m => {
     log('Client : ' + m.toString('hex'))
 })
 
-connect(0,()=>{
+connect(0, () => {
 
 });
