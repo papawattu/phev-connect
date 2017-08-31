473b5ebe915dd1b4053c5001f18fbd56b659f72f
Jamie Nuttall <jamie@wattu.com>
2017-08-31 18:08:52 +0100

    changes to logging

--- src/app.js
+++ src/app.js
@@ -1,39 +1,41 @@
-import { startPing, sendMessage, receivedMessages } from './car_service'
+import { subscribe, send, messages } from 'phev-mqtt'
 import { Observable } from 'rxjs'
-const net = require('net');
+import net from 'net'
+
+const log = message => console.log
 
 const client = new net.Socket();
 
 const connect = (cmd,cb) => {
 	if(!client.writeable) {
-		console.log('Connecting');
+		log('Connecting');
 		client.connect(8080,'192.168.8.46', ()=> {
-		});
+		})
 
 		client.on('data',(data) => {
-            sendMessage(data)
-            console.log('Car    : ' + data.toString('hex'))
-        });
+            send('phev/receive',data)
+            log('Car    : ' + data.toString('hex'))
+        })
 		client.once('connect',() => {
-			console.log('Connected');		
+			log('Connected');		
                 	cb();
 
-		});
+		})
 		client.on('error',(err) => {
-			console.log('error ' + err);
+			log('error ' + err);
 		});
 		client.on('end',() => {
-			console.log('end');
+			log('end');
 		});
 	} else {
-		comsole.log('Sending');
+		log('Sending');
 		cb();
 	}
 }
 
-receivedMessages().subscribe(m => {
+messages().subscribe(m => {
     client.write(m)
-    console.log('Client : ' + m.toString('hex'))
+    log('Client : ' + m.toString('hex'))
 })
 
 connect(0,()=>{
