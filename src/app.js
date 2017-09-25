4ce6701db1c7abe79957024e622e52e029aada9e
Jamie Nuttall <jamie@wattu.com>
2017-09-25 07:49:07 +0100

    sensible defaults to SC

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-import net from 'net'
 import SocketConnection from './socket-connection'
 import PhevManager from './phev-manager'
 import MessagingClient from './messaging-client'
 import { log } from 'phev-utils'
 
-const App = ({ messaging, client = new net.Socket(), port = 8080, host = '192.168.8.46' } = {}) => {
+const App = ({ messaging } = {}) => {
 
     log.info('Starting PHEV connect')
-    const socketConnection = SocketConnection({ client })
+    const socketConnection = SocketConnection()
     const messagingClient = MessagingClient({ messaging })
 
     PhevManager({messagingClient, socketConnection}).start()
