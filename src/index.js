187c5ccf78078c7053f88456f61abe238ece7fe6
Jamie Nuttall <jamie@wattu.com>
2017-09-26 14:04:12 +0100

    parameterised topic and subscription names

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const log = require('phev-utils').log
 
-const port      = process.env.PORT || 8080
-const host      = process.env.HOST || '192.168.8.46'
-const mqttUri   = process.env.MQTTURI || 'mqtt://secure.wattu.com'
+const port              = process.env.PORT          || 8080
+const host              = process.env.HOST          || '192.168.8.46'
+const mqttUri           = process.env.MQTTURI       || 'mqtt://secure.wattu.com'
+const topicName         = process.env.TOPIC         || 'phev/receive'
+const subscriptionName  = process.env.SUBSCRIPTION  || 'phev/send'
 
-const isMqtt    = process.env.TRANSPORT === 'mqtt'
-const messaging = isMqtt ? require('phev-messaging').MqttClient({mqttUri}) : require('phev-messaging').PubSubClient()
+const isMqtt = process.env.TRANSPORT === 'mqtt'
+const messaging = isMqtt ? require('phev-messaging').MqttClient({mqttUri, topicName, subscriptionName }) : require('phev-messaging').PubSubClient()
 
 log.info('Messaging is ' + (process.env.TRANSPORT || 'Google PubSub'))
 log.info('Server host is ' + host)
